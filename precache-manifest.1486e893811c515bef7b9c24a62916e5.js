b08d45280dbe712c3fc1aac440e27575788166d5
Jordan Myers <jordanmyers@Jordans-MBP.hitronhub.home>
2020-03-27 22:57:09 -0600

    Updates

---
+++ precache-manifest.1486e893811c515bef7b9c24a62916e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a3f9a74e93f8fc6e3479f1341c0481",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "e1b159c4e0dabe2eaec9",
+    "url": "/covid-19-tracker/static/css/main.7613bd74.chunk.css"
+  },
+  {
+    "revision": "4e7ada795bb4f1b08aab",
+    "url": "/covid-19-tracker/static/js/2.76fa8897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-tracker/static/js/2.76fa8897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b159c4e0dabe2eaec9",
+    "url": "/covid-19-tracker/static/js/main.50f2443b.chunk.js"
+  },
+  {
+    "revision": "c7ee893204412e748ec1",
+    "url": "/covid-19-tracker/static/js/runtime-main.72171ba5.js"
+  }
+]);
